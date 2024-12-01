bb4e08f5d9369edf7e093742a9f519bd99c328f9
Ben Healy <benjamin.p.healy@gmail.com>
2024-12-01 14:35:57 +0000

    Day1 Part2

---
+++ day1/src/main/java/com/ben/aoc/App.java
@@ -0,0 +1,10 @@
+package com.ben.aoc;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Day1 day1 = new Day1("input.txt");
+        System.out.println("puzzle 2: " + day1.puzzle2());
+    }
+}
